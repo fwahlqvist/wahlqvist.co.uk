fad70824f672c83b04be2b7424cca4c9fde355a8
Fredrik Wahlqvist <fwahlqvist@gmail.com>
2015-05-04 12:43:23 +0100

    2015 update
    Updated to Q2 2015

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -15,7 +15,8 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
+                        'controller' => 'Article',
+                    //  'controller' => 'Application\Controller\Index',
                         'action'     => 'index',
                     ),
                 ),
